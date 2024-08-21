1da7e574471d353a57deddaa0fdd8523b5529439
jgierlach <jegierlach@gmail.com>
2024-08-21 12:08:20 -0500

    Added filtering of outbound shipments by most recent.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,11 @@
+export const formatDate = (date) => {
+  date = new Date(date)
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0, so we need to add 1
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+}
+
 export const abbreviateString = (str, maxLength, abbreviationSymbol = '...') => {
   if (typeof str !== 'string' || typeof maxLength !== 'number' || maxLength < 1) {
     console.log('There is no Stripe Invoice Link for this contract')
--- src/routes/app/outboundshipments/+page.svelte
+++ src/routes/app/outboundshipments/+page.svelte
@@ -6,7 +6,7 @@
   import Loading from '$lib/components/Loading.svelte'
 
   // Import utility functions
-  import { abbreviateString } from '$lib/utils'
+  import { abbreviateString, formatDate } from '$lib/utils'
 
   // Import props
   export let data
@@ -21,9 +21,9 @@
 
   // Component specific variables and business logic
 
-  $: {
-    console.log('INBOUND SHIPMENTS', $outboundShipments)
-  }
+  $: outboundShipmentsByMostRecent = $outboundShipments.sort(
+    (a, b) => new Date(b.Date_Of_Last_Change) - new Date(a.Date_Of_Last_Change),
+  )
 
   let loading = false
 
@@ -44,71 +44,71 @@
 
 <Loading {loading} />
 <div class="mt-10 flex justify-center">
-  <div class="ml-10 mr-10 bg-base-100 p-4 shadow-xl">
+  <div class="ml-5 mr-5 w-full max-w-6xl bg-base-100 p-4 shadow-xl">
     <h1 class="mb-2 text-center text-3xl font-bold">Outbound Shipments</h1>
-    <!-- <div class="mb-4 flex justify-center">
-      <button
-        on:click={() => (showCreateInventory = true)}
-        class="btn btn-outline btn-primary btn-sm"
-        >Add Product <i class="fas fa-plus"></i>
-      </button>
-    </div> -->
-    <table class="table table-zebra">
-      <thead>
-        <tr>
-          <th>Product Image</th>
-          <th>Shipment Number</th>
-          <th>Carrier</th>
-          <th>Tracking Number</th>
-          <th>PO Number</th>
-          <th>Destination</th>
-          <th>Status</th>
-          <th>Order Date</th>
-          <th>Product Title</th>
-          <th>Sku</th>
-          <th>Product Quantity</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each $outboundShipments as shipment}
+    <div class="overflow-x-auto">
+      <table class="table table-zebra w-full">
+        <thead>
           <tr>
-            <td
-              ><img
-                class="h-12 w-12 rounded-md object-cover shadow-md"
-                alt="Product Image"
-                src={shipment.Product_Image_Url === null
-                  ? '/placeholder-image.jpg'
-                  : shipment.Product_Image_Url}
-              /></td
-            >
-            <td>{shipment.Shipment_Number}</td>
-            <td>{shipment.Carrier}</td>
-            <td>{shipment.Tracking_Number}</td>
-            <td>{shipment.PO_Number}</td>
-            <td>{shipment.Destination}</td>
-            <td>{shipment.Status}</td>
-            <td>{shipment.Date_Of_Last_Change}</td>
-            <td
-              ><div
-                class="tooltip"
-                role="tooltip"
-                on:mouseenter={() => handleMouseEnter(shipment['Product_Title'])}
-                on:mouseleave={handleMouseLeave}
-              >
-                {abbreviateString(shipment['Product_Title'], 25)}
-                {#if hoveredTitleId === shipment['Product_Title']}
-                  <span class="tooltip-text">
-                    {shipment['Product_Title']}...
-                  </span>
-                {/if}
-              </div></td
-            >
-            <td>{shipment.Sku}</td>
-            <td>{shipment.Quantity}</td>
+            <th>Product Image</th>
+            <th>Shipment Number</th>
+            <th>Carrier</th>
+            <th>Tracking Number</th>
+            <th>PO Number</th>
+            <th>Destination</th>
+            <th>Status</th>
+            <th>Order Date</th>
+            <th>Product Title</th>
+            <th>Sku</th>
+            <th>Product Quantity</th>
+            <th>Actions</th>
           </tr>
-        {/each}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {#each outboundShipmentsByMostRecent as shipment}
+            <tr>
+              <td>
+                <img
+                  class="h-12 w-12 rounded-md object-cover shadow-md"
+                  alt="Product Image"
+                  src={shipment.Product_Image_Url === null
+                    ? '/placeholder-image.jpg'
+                    : shipment.Product_Image_Url}
+                />
+              </td>
+              <td>{shipment.Shipment_Number}</td>
+              <td>{shipment.Carrier}</td>
+              <td>{shipment.Tracking_Number}</td>
+              <td>{shipment.PO_Number}</td>
+              <td>{shipment.Destination}</td>
+              <td>{shipment.Status}</td>
+              <td>{formatDate(shipment.Date_Of_Last_Change)}</td>
+              <td>
+                <div
+                  class="tooltip"
+                  role="tooltip"
+                  on:mouseenter={() => handleMouseEnter(shipment['Product_Title'])}
+                  on:mouseleave={handleMouseLeave}
+                >
+                  {abbreviateString(shipment['Product_Title'], 25)}
+                  {#if hoveredTitleId === shipment['Product_Title']}
+                    <span class="tooltip-text">
+                      {shipment['Product_Title']}...
+                    </span>
+                  {/if}
+                </div>
+              </td>
+              <td>{shipment.Sku}</td>
+              <td>{shipment.Quantity}</td>
+              <td class="flex justify-center">
+                <button class="btn btn-outline btn-info btn-sm mr-2">Edit</button>
+                <button class="btn btn-outline btn-primary btn-sm mr-2">Update Tracking</button>
+                <button class="btn btn-outline btn-error btn-sm">Delete</button>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
