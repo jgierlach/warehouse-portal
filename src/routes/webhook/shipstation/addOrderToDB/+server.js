b6a3b8818e5d47b084a5de5d0bea56ceccc455b4
jgierlach <jegierlach@gmail.com>
2024-09-09 16:07:13 -0500

    StoreId console.log statement.

--- src/routes/webhook/shipstation/addOrderToDB/+server.js
+++ src/routes/webhook/shipstation/addOrderToDB/+server.js
@@ -78,6 +78,8 @@ export async function POST({ request, locals }) {
 
       const storeId = advancedOptions?.storeId
 
+      console.log("STORE ID", storeId)
+
       const storeName = findStoreNameBasedOnId(storeId, stores)
 
       const clientId = assignClientIdBasedOnStoreName(storeName);
