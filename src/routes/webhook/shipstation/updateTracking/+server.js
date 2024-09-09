1a80851723de12e2b7e922ce01a2b670359530fb
jgierlach <jegierlach@gmail.com>
2024-09-09 16:37:46 -0500

    Added response object to return.

--- src/routes/webhook/shipstation/updateTracking/+server.js
+++ src/routes/webhook/shipstation/updateTracking/+server.js
@@ -16,7 +16,8 @@ export async function POST({ request, locals }) {
 
   try {
     const event = await request.json();
-    console.log('Webhook received On Fulfillment Shipped Event:', event);
+    console.log('Webhook received On Shipped Event:', event);
+    return json({ success: true }, { headers });
   } catch (err) {
     console.error('Error processing webhook:', err);
     return json({ error: 'Invalid request' }, { status: 400, headers });
