52dc1a10ce659f580aa9e2b9779b0d3342c86f31
jgierlach <jegierlach@gmail.com>
2024-09-09 16:04:44 -0500

    Added console.log to check what stores payload comes back as

--- src/routes/webhook/shipstation/addOrderToDB/+server.js
+++ src/routes/webhook/shipstation/addOrderToDB/+server.js
@@ -59,6 +59,8 @@ export async function POST({ request, locals }) {
     // Process the successful return of ShipStation stores
     const stores = await shipstationStores.json()
 
+    console.log("STORES PAYLOAD FROM SHIPSTATION", stores)
+
     // Loop through each order and process it
     for (const order of orders) {
       const {
